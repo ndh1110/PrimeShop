4303676471287ad6bd2e69c3955e26aec1577734
itsafirsttimeiveusedgit <trumcraft159@gmail.com>
2026-03-08 03:25:12 +0700

    xong README

--- obj/Debug/net8.0/1298_DuyHung.AssemblyInfo.cs
+++ obj/Debug/net8.0/1298_DuyHung.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1298_DuyHung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72209bed762371b4e1afb6471eb45a7ff5d6f6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc677b5476c4b3ddb33e37ae5760a5a7edba6ab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("1298_DuyHung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1298_DuyHung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
