048ec97561918e1bb402c036023f0afb61ff7c12
Kahprx <164650726+Kahprx@users.noreply.github.com>
2026-03-11 21:46:29 +0700

    Add files via upload

---
+++ test_now.bat
@@ -0,0 +1,4 @@
+te_path>c:/Users/kaqua/HungRZ_Shop/test_now.bat</absolute_path>
+<parameter name="content">@echo off
+"C:\Program Files\dotnet\dotnet.exe" test "C:\Users\kaqua\HungRZ_Shop\1298_DuyHung.csproj"
+pause
